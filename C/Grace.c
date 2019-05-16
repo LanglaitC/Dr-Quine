195bb4f1e41baec1864590bd12b591361fb9be8a
Corentin LANGLAIT <corentin.langlait@gmail.com>
2019-05-16 16:39:32 +0200

    Grace done

---
+++ C/Grace.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#define FILE "Grace_kid.c"
+#define CODE "#include <stdio.h>%c#define FILE %c%s%c%c#define CODE %c%s%c%c#define FT(x)int main(){fprintf(fopen(FILE, x), CODE, 10, 34, FILE, 34, 10, 34, CODE, 34, 10, 10, 34, 119, 34);}%cFT(%c%c%c)"
+#define FT(x)int main(){fprintf(fopen(FILE, x), CODE, 10, 34, FILE, 34, 10, 34, CODE, 34, 10, 10, 34, 119, 34);}
+FT("w")
