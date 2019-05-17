c962ecfb9c7e6fc37388c18bec115ce630fe9080
Corentin LANGLAIT <corentin.langlait@gmail.com>
2019-05-17 14:07:15 +0200

    comment formating

--- C/Colleen.c
+++ C/Colleen.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 void qd(void) {}
 int main(void) {
-	char *c = "#include <stdio.h>%cvoid qd(void) {}%cint main(void) {%c%cchar *c = %c%s%c;%c%cprintf(c, 10, 10, 10, 9, 34, c, 34, 10, 9, 10, 9, 10, 9, 47, 47, 10, 10, 47, 47);%c%cqd();%c%c%c* 42 *%c%c}%c%c* 42 *%c";
-	printf(c, 10, 10, 10, 9, 34, c, 34, 10, 9, 10, 9, 10, 9, 47, 47, 10, 10, 47, 47);
+	char *c = "#include <stdio.h>%cvoid qd(void) {}%cint main(void) {%c%cchar *c = %c%s%c;%c%cprintf(c, 10, 10, 10, 9, 34, c, 34, 10, 9, 10, 9, 10, 47, 10, 10, 47, 10, 10, 47, 10, 10, 47);%c%cqd();%c%c*%c42%c*%c%c}%c%c*%c42%c*%c";
+	printf(c, 10, 10, 10, 9, 34, c, 34, 10, 9, 10, 9, 10, 47, 10, 10, 47, 10, 10, 47, 10, 10, 47);
 	qd();
-	/* 42 */
+/*
+42
+*/
 }
-/* 42 */
+/*
+42
+*/
