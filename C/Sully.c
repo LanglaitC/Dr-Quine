1d19204c05ea1d9ac5ba952bfcf709a709a90921
Corentin LANGLAIT <corentin.langlait@gmail.com>
2019-05-17 17:22:44 +0200

    Sully done

--- C/Sully.c
+++ C/Sully.c
@@ -6,7 +6,7 @@ int main(){
 int i = 5;
 char t[15];
 char o[15];
-char *c = "#include <stdio.h>%c#include <string.h>%c#include <unistd.h>%cint main(){%cint i = %d;%cchar t[15];%cchar o[15];%cchar *c = %c%s%c;%cif (i) {%cstrcat(t, %cSully_%c);%ct[6] = i + 47;%ct[7] = 0;%cstrcat(t, %c.c%c);%cvoid* fd = fopen(t, %cw%c);%cfprintf(fd, c, 10, 10, 10, 10, i - 1, 10, 10, 10, 34, c, 34, 10, 10, 34, 34, 10, 10, 10, 34, 34, 10, 34, 34, 10, 10, 10, 34, 34, 34, 34, 10, 34, 34, 34, 34, 10, 10);%cfclose(fd);%cexecl(%c/usr/bin/gcc%c, %cgcc%c, t, NULL);%cexecl(%c./a.out%c, %ca.out%c, NULL);%c}%c}";
+char *c = "#include <stdio.h>%c#include <string.h>%c#include <unistd.h>%c#include <stdlib.h>%cint main(){%cint i = %d;%cchar t[15];%cchar o[15];%cchar *c = %c%s%c;%cif (i) {%cstrcat(t, %cSully_%c);%ct[6] = i + 47;%ct[7] = 0;%cstrcpy(o, t);%cstrcat(t, %c.c%c);%cvoid* fd = fopen(t, %cw%c);%cfprintf(fd, c, 10, 10, 10, 10, 10, i - 1, 10, 10, 10, 34, c, 34, 10, 10, 34, 34, 10, 10, 10, 10, 34, 34, 10, 34, 34, 10, 10, 10, 10, 34, 37, 37, 37, 34, 10, 10, 10);%cfclose(fd);%cchar cmd[500];%csprintf(cmd, %cgcc -o %cs %cs && ./%cs%c, o, t, o);%csystem(cmd);%c}%c}";
 if (i) {
 strcat(t, "Sully_");
 t[6] = i + 47;
@@ -14,9 +14,10 @@ t[7] = 0;
 strcpy(o, t);
 strcat(t, ".c");
 void* fd = fopen(t, "w");
-fprintf(fd, c, 10, 10, 10, 10, i - 1, 10, 10, 10, 34, c, 34, 10, 10, 34, 34, 10, 10, 10, 34, 34, 10, 34, 34, 10, 10, 10, 34, 34, 34, 34, 10, 34, 34, 34, 34, 10, 10);
+fprintf(fd, c, 10, 10, 10, 10, 10, i - 1, 10, 10, 10, 34, c, 34, 10, 10, 34, 34, 10, 10, 10, 10, 34, 34, 10, 34, 34, 10, 10, 10, 10, 34, 37, 37, 37, 34, 10, 10, 10);
 fclose(fd);
-execl("/usr/bin/gcc", "gcc", "-o", o, t, NULL);
-execl("./a.out", "a.out", NULL);
+char cmd[500];
+sprintf(cmd, "gcc -o %s %s && ./%s", o, t, o);
+system(cmd);
 }
 }
